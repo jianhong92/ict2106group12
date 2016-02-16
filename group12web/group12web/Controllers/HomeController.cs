f23bcf8457715f80ecaf48946b68ebe0450cb70c
teoweiwei <teoweisquare@outlook.com>
2016-02-16 20:41:51 +0800

    Add upload CSV function without DB

--- group12web/group12web/Controllers/HomeController.cs
+++ group12web/group12web/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using group12web.Models;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -25,6 +28,53 @@ namespace group12web.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Upload(HttpPostedFileBase upload)
+        {
+            if (ModelState.IsValid)
+            {
+                if (upload != null && upload.ContentLength > 0)
+                {
+
+                    if (upload.FileName.EndsWith(".csv"))
+                    {
+                        Stream stream = upload.InputStream;
+                        StreamReader read = new StreamReader(stream);
+
+                        List<string> row = new List<string>();
+                        Weather weather = new Weather();
+                        read.ReadLine();// Read off header
+
+                        while (!read.EndOfStream)
+                        {
+                            row = read.ReadLine().Split(',').ToList();
+
+                            weather.Station = row[0];
+                            weather.Date = DateTime.ParseExact(row[3] + "/" + row[2] + "/" + row[1], "d/M/yyyy", CultureInfo.InvariantCulture); ;
+                            weather.Rainfall = decimal.Parse(row[4]);
+
+                            //Add record into DB
+                            //dbContext.Weathers.Add(weather);
+                            //dbContext.SaveChanges();
+                        }
+                        ViewBag.csv = "<p>" + row[1] + "</p>";
+                        return View();
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("File", "This file format is not supported");
+                        return View("Index");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("File", "Please Upload Your file");
+                }
+            }
+            return View("Index");
         }
     }
 }
