cf36308096e85147c4d8cce37571c02b89aaae18
Saaabri <sabb.info@gmail.com>
2016-02-22 02:50:31 +0800

    added a line in global.asax.cs to initialize the modals

--- group12web/group12web/Global.asax.cs
+++ group12web/group12web/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using group12web.DAL;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +18,7 @@ namespace group12web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Database.SetInitializer<group12Context>(null);
         }
     }
 }
